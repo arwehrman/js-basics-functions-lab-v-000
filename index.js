a309fdca7b9629c1ef6acbc0a2f6cdbf024e356d
Amanda Wehrman <arwehrman@gmail.com>
2018-01-07 21:18:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+ if (location > 42) { return location - 42
+ }
+ else {
+   return 42 - location
+ }
+}
+
+function distanceFromHqInFeet(location){
+  return (distanceFromHqInBlocks(location)) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+  if (start > destination) {
+  return  (start - destination) * 264
+  }
+  else {
+  return (destination - start) * 264
+  }
+}
+
+function calculatesFarePrice(start, destination){
+  if ((distanceFromHqInFeet(start, destination)) >= 2500) {
+    return 'cannot travel that far'
+  }
+}
